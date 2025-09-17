55a96ebe5a1e1c540d766bf696a54ef7b1303dd5
akiirafilia@gmail.com <pat-escano>
2025-09-17 12:11:40 +0700

    added pokemon collection array

--- pokeapi.js
+++ pokeapi.js
@@ -1,7 +1,6 @@
 async function getPokeData(){
-    const limit = '151';
     const pokemons = [];
-    const url = `https://pokeapi.co/api/v2/pokemon/ditto`;
+    const url = `https://pokeapi.co/api/v2/pokemon/`;
 
     try {
         const response = await fetch(url);
@@ -10,18 +9,30 @@ async function getPokeData(){
         }
 
         const result = await response.json();
-        console.log(result.sprites);
-        document.getElementById('body').innerHTML += `<img src="${result.sprites.front_default}" />`
-        for (let i = 0; i < limit; i++ ){
-            pokemons.push(result.results[i].name);
+        console.log(result);
+        // document.getElementById('header').innerHTML += `<img src="${result.sprites.front_default}" />`
+        for (let i = 0; i < result.results.length; i++ ){
+           const pokeResponse = await fetch(result.results[i].url);
+           const pokeData = await pokeResponse.json();
+
+           pokemons.push({
+                id: pokeData.id,
+                name: pokeData.name,
+                sprite: pokeData.sprites.front_default,
+           })
         }
 
-        for (let i = 0; i < limit; i++){
-            document.getElementById('body').innerHTML += `
-            
-            <p>${pokemons[i]}</p>
-            <img src="https://github.com/PokeAPI/sprites/${pokemons[i]}/other/home" />`
+        console.log(pokemons);
+        
+        for (let i = 0; i < pokemons.length; i++){
+            document.getElementById('gridGallery').innerHTML += `
+            <div id="${pokemons[i].id}">
+                <p>${pokemons[i].name}</p>
+                <img src="${pokemons[i].sprite}" />
+            </div>
+            `
         }
+       
     } catch(error){
         console.error(error.message);
     }
