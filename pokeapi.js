dac02bac9df99ea6ea1fef57d3553fd665146b50
akiirafilia@gmail.com <pat-escano>
2025-09-19 17:21:00 +0700

    added pokemon types

--- pokeapi.js
+++ pokeapi.js
@@ -10,24 +10,51 @@ async function getPokeData(){
 
         const result = await response.json();
         console.log(result);
-        // document.getElementById('header').innerHTML += `<img src="${result.sprites.front_default}" />`
+
         for (let i = 0; i < result.results.length; i++ ){
            const pokeResponse = await fetch(result.results[i].url);
+        
            const pokeData = await pokeResponse.json();
 
+           const pokeTypes = pokeData.types;
+           console.log(pokeTypes);
+           
+           const typeNames = pokeTypes.map(item => item.type.name);
+           console.log(typeNames);
+
+           
            pokemons.push({
                 id: pokeData.id,
                 name: pokeData.name,
+                types: typeNames,
                 sprite: pokeData.sprites.front_default,
            })
+           
+           console.log(pokemons);
         }
 
         console.log(pokemons);
         
         for (let i = 0; i < pokemons.length; i++){
+            const pokemonTypes = pokemons[i].types;
+
+            let typeHTML = '';
+
+            if (pokemonTypes.length == 1) {
+                typeHTML = `<div>${pokemonTypes[0]}</div>`
+            } else {
+                typeHTML = `
+                <div>${pokemonTypes[0]}</div>
+                <div>${pokemonTypes[1]}</div>
+                `
+            }
+
             document.getElementById('gridGallery').innerHTML += `
             <a id="${pokemons[i].id}" class="border flex flex-col items-center rounded-lg hover:scale-125 clickable">
                 <img src="${pokemons[i].sprite}" />
+                <div class="flex gap-2">
+                    ${typeHTML}
+                </div>
                 <p class="capitalize">${pokemons[i].name}</p>
             </a>
             `;
